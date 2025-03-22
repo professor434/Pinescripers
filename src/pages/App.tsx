10cc6ec096e6897d945f9814de4f31708c071246
professor434 <81698289+professor434@users.noreply.github.com>
2025-03-22 23:20:54 +0200

    Update App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from '../pages/Login';
 import DevPanel from '../components/DevPanel';
-import Layout from '../components/Layout'; // πρόσθεσε αυτό
 
 const App = () => {
   return (
-    <Layout>
-      <h1>Welcome to Pinescripers</h1>
-      <DevPanel />
-    </Layout>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<DevPanel />} />
+    </Routes>
   );
 };
 
